20e315677fcfac220272ebd8b159c346ce7f16f5
Alexandre P <alexandre.perrot54@gmail.com>
2023-02-16 22:41:57 +0100

    index

--- src/views/Header.php
+++ src/views/Header.php
@@ -49,14 +49,14 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-menu" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fa fa-bars"></i>
                 </button>
-                <a class="navbar-brand" href="index.html"><img src="images/logo.png" class="logo" alt=""></a>
+                <a class="navbar-brand" href="?action="><img src="images/logo.png" class="logo" alt=""></a>
             </div>
             <!-- End Header Navigation -->
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="navbar-menu">
                 <ul class="nav navbar-nav ml-auto" data-in="fadeInDown" data-out="fadeOutUp">
-                    <li class="nav-item"><a class="nav-link" href="">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="?action=">Accueil</a></li>
                     <li class="nav-item"><a class="nav-link" href="?action=about-us">A propos</a></li>
                     <li class="nav-item"><a class="nav-link" href="?action=shop">Boutique</a></li>
                 </ul>
