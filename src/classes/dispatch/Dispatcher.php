0a9d94667abeb7bd23f5a036901c5c0cac048185
adrien99920 <adrien.scher@gmail.com>
2023-02-16 18:49:51 +0100

    Ajout connexion, inscription

--- src/classes/dispatch/Dispatcher.php
+++ src/classes/dispatch/Dispatcher.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Application\dispatch;
 
+use Application\action\InscriptionAction;
+use Application\action\LogConnAction;
 use Application\action\LoginAction;
 
 use Application\action\AddCartAction;
@@ -29,11 +31,6 @@ class Dispatcher
     final public function dispatch(): void
     {
         switch ($this->action) {
-            case 'login':
-                $act = new LoginAction();
-                $act->execute();
-                break;
-
             case 'shop':
                 $action = new ShopAction();
                 $action->execute();
@@ -43,8 +40,18 @@ class Dispatcher
                 $act = new ViewProductAction();
                 $act->execute();
                 break;
+            case 'conn_log':
+                $act = new LogConnAction();
+                $act->execute();
+                break;
             case 'login':
                 $act = new LoginAction();
+                $act->execute();
+                break;
+            case 'inscription':
+                $act = new InscriptionAction();
+                $act->execute();
+                break;
             case 'cart':
                 $act = new CartAction();
                 $act->execute();
