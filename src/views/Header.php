296b1d17c493eb9b60fe3dc66be2ba5c8eaa3126
Leonarddoo <samuel.pomin@gmail.com>
2023-02-16 18:55:13 +0100

    Ajout de MonCompte

--- src/views/Header.php
+++ src/views/Header.php
@@ -65,14 +65,22 @@
 
             <!-- Start Atribute Navigation -->
             <div class="attr-nav">
-                <ul>
-                    <li class="side-menu"><a href="#">
-                            <i class="fa fa-shopping-bag"></i>
-                            <span class="badge">3</span>
-                            <p>Mon Panier</p>
-                        </a></li>
-                </ul>
-            </div>
+                    <ul>
+                        <li class="side-menu">
+                            <a href="#">
+                                <i class="fa fa-shopping-bag"></i>
+                                <span class="badge">3</span>
+                                <p>Mon Panier</p>
+					        </a>
+                        </li>
+                        <li class="side-menu">
+                            <a href="login.html">
+                                <i class="fa fa-user"></i>
+                                <p>Mon Compte</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
 
             <div class="login">
                 <a href="?action=login"><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16" style="color: black"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/></svg></a>
